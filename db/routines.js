a4cfe57a750d939d5706b45f0b94d69d7074a4e1
DannyReese <113713130+DannyReese@users.noreply.github.com>
2023-01-26 21:38:36 -0500

    ok routines working

--- db/routines.js
+++ db/routines.js
@@ -2,13 +2,13 @@ const client = require("./client");
 
 async function createRoutine({ creatorId, isPublic, name, goal }) {
   try{
-    const {rows : [routine]} = await client.query(`
+    const {rows : routine} = await client.query(`
     INSERT INTO routines
     ("creatorId", "isPublic", name, goal)
     VALUES
-    ($1,$2,$3,$4);`,[creatorId,isPublic,name,goal]);
+    ($1,$2,$3,$4) RETURNING *;`,[creatorId,isPublic,name,goal]);
 
-    return [routine]
+    return routine
   }catch(error){throw new Error('no routine made')}
 }
 
