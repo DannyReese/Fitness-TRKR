76ee4e3f2cd792feb76fa005756ba52c97b683c2
Raven Ontiveros <ravenontiveros@Ravens-MacBook-Air.local>
2023-02-08 15:24:33 -0700

    need to figure out issues for routine activites

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -7,7 +7,7 @@ async function addActivityToRoutine({
   duration,
 }){
   try{
- const {rows:routineActivity} = await client.query(`
+ const { rows:routineActivity } = await client.query(`
  INSERT INTO "routineActivites"(
   "routineId",
   "activityId",
@@ -27,17 +27,101 @@ async function addActivityToRoutine({
 
 async function getRoutineActivityById(id) {
   try{
-    
+    const { rows:routineActivity } = await client.query(`
+    SELECT "routineId" 
+    FROM routine_activities 
+    WHERE id = $1
+    RETURNING *;`,
+    [id]);
+
+   return routineActivity;
+ } catch(error){
+   throw new Error('cannot get routine_activity by the id')
   }
 }
 
-async function getRoutineActivitiesByRoutine({ id }) {}
+async function getRoutineActivitiesByRoutine({ id }) {
+  try{
+    const { rows:routineActivity } = await client.query(`
+    SELECT *
+    FROM routine_activities 
+    WHERE "routineId" = ${id}`);
 
-async function updateRoutineActivity({ id, ...fields }) {}
+    return routineActivity;
+  } catch(error){
+    throw new Error('cannot get routine_activity by the routine')
+  }
+}
 
-async function destroyRoutineActivity(id) {}
+async function updateRoutineActivity({ id, ...fields }) {
+ try{
+  const { count, duration } = fields
+  let updateFields 
 
-async function canEditRoutineActivity(routineActivityId, userId) {}
+  if(count) {
+    const { rows:[updatedRoutineActivity] } = await client.query(`
+    UPDEATE routine_activites 
+    SET count = $1
+    WHERE id=$2
+    RETURNING *;`,
+    [count, id]);
+
+    returnValue = updatedRoutineActivity
+  }
+
+ if (duration){
+  const { rows:[updatedRoutineActivity] } = await client.query(`
+  UPDATE routine_activities
+  SET duration = $1
+  WHERE id=$2
+  RETURNING *;`,
+  [duration,id]);
+
+  returnValue = updatedRoutineActivity
+ }
+
+  return returnValue;
+} catch(error){
+  throw new Error('cannot update the routine_activity ')
+}
+
+async function destroyRoutineActivity(id) {
+  try{
+    const { rows: [deletedRoutine] } = await client.query(`
+    DELETE FROM routine_activites 
+    WHERE id=$1
+    RETURNING *;`,
+    [id]);
+
+    return deletedRoutine
+  } catch(error){
+    throw new Error('unable to delete the routine activity')
+  }
+}
+
+async function canEditRoutineActivity(routineActivityId, userId) {
+  try{
+    const { rows: [routineId] } = await client.query(`
+    SELECT "routineId"
+    FROM routine_activites
+    WHERE id=$1;`,
+    [routineActivityId]);
+
+    const { rows: [routineCreatorId] } = await client.query(`
+    SELECT "creatorId"
+    FROM routines
+    WHERE id=$1;`,
+    [routineId.routineId]);
+
+    if (routineCreatorId.creatorId === userId) {
+      return true
+    } else {
+      return false
+    }
+  } catch(error) {
+    throw new Error('cannot edit answer')
+  }
+}
 
 module.exports = {
   getRoutineActivityById,
