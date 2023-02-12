12f0ed9bd3323602130f484b77773e2f63e098f8
carowoline <caro98.jpg@gmail.com>
2023-02-11 21:18:55 -0500

    PATCH routineActivities

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,9 +1,31 @@
 const express = require('express');
-const router = express.Router();
+const { getRoutineActivityById, getRoutineById, updateRoutineActivity } = require('../db');
 const routineActivitiesRouter = express.Router();
 
 // PATCH /api/routine_activities/:routineActivityId
+routineActivitiesRouter.patch('/:routineActivityId', async (req, res, next) => {
+    try {
+        const { routineActivityId } = req.params;
+        const { count, duration } = req.body;
+        const routineActivity = await getRoutineActivityById(routineActivityId);
+        const { routineId } = routineActivity;
+        const routine = await getRoutineById(routineId);
+        const updated = await updateRoutineActivity({ id: routineActivityId, count, duration });
 
+        if (!req.user || req.user.id !== routine.creatorId) {
+            res.send({
+                error: 'CreatorError',
+                message: 'Must be creator'
+            });
+            return;
+        }
+
+        res.send(updated);
+    }
+    catch (error) {
+        next(error)
+    }
+});
 
 // DELETE /api/routine_activities/:routineActivityId
 
