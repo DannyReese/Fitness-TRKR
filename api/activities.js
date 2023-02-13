f115084517b8c4aeff3ac7d66982db17fefefadf
carowoline <caro98.jpg@gmail.com>
2023-02-12 21:59:26 -0500

    api

--- api/activities.js
+++ api/activities.js
@@ -3,29 +3,98 @@ const { getPublicRoutinesByActivity } = require('../db');
 const activitiesRouter = express.Router();
 
 // GET /api/activities/:activityId/routines
-activitiesRouter.get(`/:activityId/routines`, async (req, res, next) => {
-    try {
-        const activities = await getPublicRoutinesByActivity({ id: req.params.activityId });
+activitiesRouter.get('/:activityId/routines', async (req, res, next) => {
 
-        if (activities) {
+    try {
+        const activityId = req.params;
+        activityId.id = parseInt(activityId.activityId);
+        const activities = await getPublicRoutinesByActivity(activityId);
+
+        if (activities.length) {
             res.send(activities);
-        }
-        else {
-            next({
+        } else {
+            res.send({
+                error: "error",
+                message: `Activity ${req.params.id} not found`,
                 name: 'name'
-                message: `Activity ${activities} not found`
-            })
+            });
         }
+
     }
     catch (error) {
+        next(error)
+
+    }
+
+});
+
+// GET /api/activities
+activitiesRouter.get('/', async (req, res, next) => {
+    try {
+        const allActivities = await getAllActivities();
+
+        res.send(allActivities);
+    } catch (error) {
         next(error)
     }
 });
 
-// GET /api/activities
-
 // POST /api/activities
+router.post('/', async (req, res, next) => {
+    try {
+        if (req.user) {
+            const { name, description } = req.body;
+            const activitieCheck = await getActivityByName(name);
+
+            if (activitieCheck) {
+                res.send({
+                    error: "error activity already exists",
+                    message: "An activity with name Push Ups already exists",
+                    name: "ActivityExists"
+                })
+            } else {
+                const activity = await createActivity({ name, description });
+
+                res.send(activity);
+            }
+        }
+    } catch (error) {
+        next(error)
+    }
+});
 
 // PATCH /api/activities/:activityId
+router.patch('/:activityId', async (req, res,next) => {
+    try{
+    const fields = req.body;
+    const id = req.params.activityId;
+    const name = fields.name;
+    const checkActivity = await getActivityById(id);
+    const checkActivityName = await getActivityByName(name);
+    const updatedActivity = {};
+    
+    if (checkActivity) {
+        (checkActivityName) ?
+            res.send({
+                error: 'error',
+                message: `An activity with name ${name} already exists`,
+                name: 'NameTaken'
+            })
+            :
+            updatedActivity.activity = await updateActivity({ id, name: fields.name, description: fields.description });
+        res.send(updatedActivity.activity)
+
+    } else {
+        res.send({
+            error: 'error',
+            message: `Activity ${id} not found`,
+            name: 'errorMessage'
+        });
+    }
+    }catch(error){
+        next(error)
+    }
+})
+
 
 module.exports = activitiesRouter;
--- api/users.js
+++ api/users.js
@@ -55,9 +55,77 @@ usersRouter.post('/register', async (req, res, next) => {
 });
 
 // POST /api/users/login
+usersRouter.post('/login', async (req, res,next) => {
+    const { username, password } = req.body;
+
+    if (!username || !password) {
+        res.send({
+            name: 'MissingUserOrPassword',
+            message: 'Must enter username and password',
+            error: 'error'
+        });
+    }
+    try {
+
+        const user = await getUser({ username, password });
+
+        if (user) {
+            const jwt = require('jsonwebtoken');
+            const token = jwt.sign({ id: user.id, username: user.username }, process.env.JWT_SECRET);
+            res.send({ message: "you're logged in!", token, user });
+
+        } else {
+            res.send({
+                name: 'incorrect',
+                message: "incorrect user name or password"
+            });
+        }
+
+    } catch (error) {
+      next(error)
+    }
+});
 
 // GET /api/users/me
+usersRouter.get('/me', async (req, res, next) => {
+    try {
+        const user = req.user;
+
+        if (user) {
+            const { id } = req.user;
+            const user = await getUserById(id);
+            res.send(user);
+        }
+        if (!user) {
+            res.status(401).send({
+                error: "NotValidUser",
+                message: "You must be logged in to perform this action",
+                name: "Unauthorized"
+            });
+        }
+
+    } catch (error) {
+       next(error)
+    }
+});
 
 // GET /api/users/:username/routines
+usersRouter.get('/:username/routines', async (req, res,next) => {
+    
+    try {
+    const user = req.user;
+    const username = req.params.username;
+        if (username === user.username) {
+            const routines = await getAllRoutinesByUser(user);
+            res.send(routines);
+        } else {
+            const routines = await getPublicRoutinesByUser({username});
+            res.send(routines);
+
+        }
+    } catch (error) {
+        next(error)
+    }
+});
 
 module.exports = usersRouter;
